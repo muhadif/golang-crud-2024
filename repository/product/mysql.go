95e98e1a678440b4a9ff34e996a8c01f8ad79696
muhadif <muhadifff@gmail.com>
2024-05-31 11:07:29 +0700

    fix (product) : add validation stock for product cart

--- repository/product/mysql.go
+++ repository/product/mysql.go
@@ -2,8 +2,11 @@ package product
 
 import (
 	"context"
+	"errors"
 	"golang-crud-2024/core/entity"
+	coreErr "golang-crud-2024/core/error"
 	"golang-crud-2024/core/repository"
+	"golang-crud-2024/pkg/fault"
 	"gorm.io/gorm"
 )
 
@@ -15,6 +18,19 @@ func NewRepository(db *gorm.DB) repository.ProductRepository {
 
 type repo struct {
 	db *gorm.DB
+}
+
+func (r repo) GetProductBySerial(ctx context.Context, serial string) (*entity.Product, error) {
+	var product *entity.Product
+	if err := r.db.WithContext(ctx).Where("serial = ?", serial).First(&product).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fault.ErrorDictionary(fault.HTTPNotFound, coreErr.ErrProductNotFound)
+		}
+
+		return nil, err
+	}
+
+	return product, nil
 }
 
 func (r repo) GetProduct(ctx context.Context, req entity.GetProductRequest) ([]*entity.Product, error) {
